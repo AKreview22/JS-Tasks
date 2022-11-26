80dd7c35a513d44a18b8e374f608dc995444bfdc
Ahmed Kamal <56924629+AKreview22@users.noreply.github.com>
2022-11-26 11:19:47 +0200

    Add files via upload

---
+++ Lab 6 Tasks ( Day 8 )/Changing Text style/src/script.js
@@ -0,0 +1,40 @@
+let par = document.getElementById("PAR");
+
+function ChangeFont(font) {
+
+    par.style.fontFamily = font;
+}
+
+function ChangeAlign(align) {
+
+    par.style.textAlign = align;
+}
+
+function ChangeHeight(height) {
+    par.style.lineHeight = height;
+}
+
+function ChangeLSpace(space) {
+
+    par.style.letterSpacing = space;
+}
+
+function ChangeIndent(indent) {
+    par.style.textIndent = indent;
+}
+
+function ChangeTransform(transformCase) {
+    par.style.textTransform = transformCase;
+}
+
+function ChangeDecorate(decoration) {
+    par.style.textDecoration = decoration;
+}
+
+function ChangeBorder(border) {
+    par.style.borderStyle = border;
+}
+
+function ChangeBorderColor(borderColor) {
+    par.style.borderColor = borderColor;
+}
